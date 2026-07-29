7fc11940cab782f2b5dbca862716450f9744f9a1
Alan O <alan.oo@education.nsw.gov.au>
2026-07-29 23:39:57 +0000

    feature add: sketches

--- 01.serialMonitor/01.serialMonitor.ino
+++ 01.serialMonitor/01.serialMonitor.ino
@@ -28,12 +28,21 @@
     https://docs.arduino.cc/software/ide-v2/tutorials/ide-v2-serial-plotter
 */
 
-// The setup function runs once when you press reset or power the board
 void setup() {
-
+  Serial.begin(115200);                  // Start talking to the computer at 115200 baud
+  Serial.print("Count: ");
+Serial.println(42);   // Prints: Count: 42
 }
 
-// The loop function runs over and over again forever
 void loop() {
-
+  Serial.print("monkee");
+  delay(1000);  // Wait 1 second each cycle
 }
+
+void loop() {
+  Serial.print("monkee");
+  delay(1000);  // Wait 1 second each cycle
+}
+
+
+
